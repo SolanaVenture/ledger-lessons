c3302b1da10cdcbe435b719e23782a564c3eb897
Park Lu <chochinlu@gmail.com>
2024-04-24 17:09:32 +0800

    add the enroll functions

--- programs/ledger-lessons/src/lib.rs
+++ programs/ledger-lessons/src/lib.rs
@@ -5,6 +5,8 @@ declare_id!("9XKM2fDpipiqeYSNFGi43cVuS888EpsVbhqYJmymiDCD");
 
 #[program]
 pub mod ledger_lessons {
+    use anchor_lang::solana_program::system_instruction;
+
     use super::*;
 
     // Initialize the course and set up the corresponding escrow account
@@ -32,8 +34,38 @@ pub mod ledger_lessons {
     }
 
     // The students register to join the course
-    pub fn enroll(_ctx: Context<Enroll>, _deposit_amount: u64) -> Result<()> {
-        // TODO
+    pub fn enroll(ctx: Context<Enroll>, _deposit_amount: u64) -> Result<()> {
+        let escrow = &mut ctx.accounts.escrow;
+        let student = &mut ctx.accounts.student;
+        let course = &ctx.accounts.course;
+
+        let deposit_amount = course.deposit_amount; //lamports
+
+        let student_balance = student.to_account_info().lamports();
+        require!(
+            student_balance > deposit_amount,
+            LedgerClassError::InsufficientFunds
+        );
+
+        // Transfer the deposit from the student account to the escrow account.
+        // TODO: need to confirm it !
+        let transfer_instruction = system_instruction::transfer(
+            &student.to_account_info().key(),
+            &escrow.to_account_info().key(),
+            deposit_amount,
+        );
+        anchor_lang::solana_program::program::invoke_signed(
+            &transfer_instruction,
+            &[
+                student.to_account_info(),
+                escrow.to_account_info(),
+                ctx.accounts.system_program.to_account_info(),
+            ],
+            &[],
+        )?;
+
+        escrow.deposits.push((student.key(), deposit_amount));
+
         Ok(())
     }
 
@@ -89,8 +121,26 @@ pub struct Initialize<'info> {
 }
 
 #[derive(Accounts)]
-pub struct Enroll {
-    // TODO
+pub struct Enroll<'info> {
+    #[account(mut)]
+    pub student: Account<'info, StudentAccount>,
+
+    #[account(mut)]
+    pub escrow: Account<'info, EscrowAccount>,
+
+    #[account(mut)]
+    pub course: Account<'info, CourseAccount>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[error_code]
+pub enum LedgerClassError {
+    #[msg("The balance of the student account should be greater than the deposit amount for that course.")]
+    InsufficientFunds,
 }
 
 #[derive(Accounts)]
@@ -119,7 +169,7 @@ pub struct CourseAccount {
     pub name: String,
     pub description: String,
     pub total_hours: u64,
-    pub deposit_amount: u64, // The deposit that each student should pay
+    pub deposit_amount: u64, // The deposit that each student should pay (lamports)
     pub students: Vec<Pubkey>,
     pub teacher: Pubkey,
 }
